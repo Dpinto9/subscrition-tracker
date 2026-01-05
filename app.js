7a95d360d61d59f75cf9a2ff09ae340e23d2a3b8
Diogo Pinto <Diogo.a.p.pinto@gmail.com>
2026-01-05 23:26:27 +0000

    Backend subscription-tracker

---
+++ app.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import { PORT } from './config/env.js';
+
+import userRouter from './routes/user.routes.js';
+import authRouter from './routes/auth.routes.js';
+import subscriptionRouter from './routes/subscription.routes.js';
+
+import connectDB from './database/mongobd.js';
+import errorMiddleware from './middleware/error.middleware.js';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+app.use(cookieParser());
+
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/subscriptions', subscriptionRouter);
+
+app.use(errorMiddleware);
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the subscription tracker api!');
+});
+
+app.listen( PORT, async() => {
+    console.log(`Server is running on port http://localhost:${PORT}`);
+
+    await connectDB();
+})
+
+export default app;
---
+++ routes/subscription.routes.js
@@ -0,0 +1,38 @@
+import { Router } from 'express';
+
+const subscriptionRouter = Router();
+
+subscriptionRouter.get( '/', ( req, res ) => {
+    res.send( 'Get all subscriptions' );
+});
+
+subscriptionRouter.get( '/:id', ( req, res ) => {
+    res.send( 'Get subscription details' );
+});
+
+subscriptionRouter.post( '/', ( req, res ) => {
+    res.send( 'Create subscriptions' );
+});
+
+subscriptionRouter.put( '/:id', ( req, res ) => {
+    res.send( 'Update subscriptions' );
+});
+
+subscriptionRouter.delete( '/:id', ( req, res ) => {
+    res.send( 'Delete subscriptions' );
+});
+
+subscriptionRouter.get( '/user/:id', ( req, res ) => {
+    res.send( 'Get user subscriptions' );
+});
+
+subscriptionRouter.put( '/:id/cancel', ( req, res ) => {
+    res.send( 'Cancel subscription' );
+});
+
+subscriptionRouter.get( '/upcoming-renewals', ( req, res ) => {
+    res.send( 'Get upcoming renewals' );
+});
+
+
+export default subscriptionRouter;
